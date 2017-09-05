fd97c9a93b2da57300ef31f4d56c6e3d5d7f7b21
richardk88 <richardk88@hotmail.com>
2017-09-05 15:50:48 -0400

    pushing to heroku

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -4,48 +4,58 @@ class Api::ArtistsController < ApplicationController
         render json: @artists
     end
     
-    def create
-        @artist = Artist.new(artist_params)
-        if @artist.save
-            render json: @artist
-        else
-            render json: {
-                message: 'Error when creating Artist'
-            }
+    class Api::ArtistsController < ApplicationController
+        def index
+          @artists = Artist.all
+          render json: @artists
         end
-        # redirect_to artist_path(@artist)
-      end
-    
-      def show
-        @artist = Artist.find(params[:id])
-        render json: @artist
-      end
-    
-      def update
-        @artist = Artist.find(params[:id])
-        if @artist.update(artist_params)
-            render json: @artist
-        else
-            render json: {
-                message: 'Error when creating Artist'
-            }
+      
+        def show
+          @artist = Artist.find(params[:id])
+          @songs = @artist.songs.all
+          render json: {
+            artist: @artist,
+            songs: @songs
+          }
         end
-        # redirect_to artist_path(@artist)
-      end
-    
-      def destroy
-        @artist = Artist.find(params[:id])
-        @artist.destroy
-        
-        render json: {
+      
+        def create
+          @artist = Artist.new artist_params
+      
+          if @artist.save
+            render json: @artist
+          else
+            render json: {
+              message: 'Error when creating Artist'
+            }
+          end
+        end
+      
+        def update
+          @artist = Artist.find params[:id]
+      
+          if @artist.update(artist_params)
+            render json: @artist
+          else
+            render json: {
+              message: 'Error when updating Artist'
+            }
+          end
+        end
+      
+        def destroy
+          @artist = Artist.find(params[:id])
+          @artist.destroy
+      
+          render json: {
             message: 'Artist successfully destroyed'
-        }
-        # redirect_to artists_path
-      end
-    
-      private
-    
-      def artist_params
-        params.require(:artist).permit(:name, :photo_url, :nationality)
+          }
+        end
+      
+        private
+      
+        def artist_params
+          params.require(:artist).permit(:name, :photo_url, :nationality)
+        end
       end
 end
